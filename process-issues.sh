232a54a64702ce40ce4381f22aedeff9e2514e84
Trenton D. Adams <trent@trentonadams.ca>
2011-01-12 20:37:00 -0700

    - fix bug that wasn't tested in the process-issues.sh

--- process-issues.sh
+++ process-issues.sh
@@ -3,8 +3,8 @@ GITHUB_TOKEN=$(git config github.token)
 # test url
 #URL="http://github.com/api/v2/xml/issues/open/TrentonAdams/testing"
 #LABEL_URL="http://github.com/api/v2/xml/issues/label/add/TrentonAdams/testing/"
-OPEN_URL="http://github.com/api/v2/xml/issues/open/TrentonAdams/lpa"
-LABEL_URL="http://github.com/api/v2/xml/issues/label/add/TrentonAdams/lpa/"
+OPEN_URL="https://github.com/api/v2/xml/issues/open/TrentonAdams/lpa"
+LABEL_URL="https://github.com/api/v2/xml/issues/label/add/TrentonAdams/lpa/"
 
 # WARNING, WARNING, WARNING major limitation in this script.  You cannot create multiple issues from the same file, as they get created as a single issue
 FILES="$( egrep -rl 'new-issue{.+}' ./* | egrep -v 'process-issues|README')"
@@ -12,7 +12,7 @@ for i in $FILES; do
   cp $i ${i}.newissue;
   eval $(egrep 'new-issue{.*}' ${i}.newissue | perl -e 'while (<>) { s/^.*new-issue{(.*)}.*$/$1/; print "$_\n"; }')
   issue=$(curl -F 'login=TrentonAdams' -F "token=$GITHUB_TOKEN" -F "title=$title" -F "body=$content" \
-    $URL 2>/dev/null | xpath -e '/issue/number/text()' 2>/dev/null)
+    $OPEN_URL 2>/dev/null | xpath -e '/issue/number/text()' 2>/dev/null)
   cat ${i}.newissue | perl -e 'while (<>) { s/new-issue{.*}/'"$title"' (issue-'$issue')/; print "$_"; }' > ${i}
   for l in $label; do 
     curl -F 'login=TrentonAdams' -F "token=$GITHUB_TOKEN" \
